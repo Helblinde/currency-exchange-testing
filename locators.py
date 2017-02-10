abc2cae9034430833847917c70a40f3d0ad97623
Helblinde <helblinde92@gmail.com>
2017-02-11 01:44:43 +0300

    XPath locators have been updated

--- locators.py
+++ locators.py
@@ -3,25 +3,21 @@ from selenium.webdriver.common.by import By
 
 class ConverterPageLocators(object):
     """A class for main page locators. All main page locators should come here"""
-    INPUT_FIELD = (By.XPATH, "//input[@placeholder='Сумма']")
+
+    INPUT_FIELD = (By.XPATH, "//aside//input")
     FROM_TYPE_CASH = (By.XPATH, "//p[text()='Наличные']")
     TO_TYPE_CASH = (By.XPATH, "//p[text()='Выдать наличные']")
-    SUBMIT_BUTTON = (By.XPATH, "//aside[@class='rates-aside']//"
-                               "button[text()='Показать']")
-    RESULT_VALUE = (By.XPATH, "//div[@class='converter-result']/h4/span[1]")
-    RESULT_CURRENCY = (By.XPATH, "//div[@class='converter-result']/h4/span[2]")
-    BUY_EXCHANGE_RATE = (By.XPATH, "//div[@class='current-table']/table/tbody/"
-                                   "tr/td[3]/span")
-    SELL_EXCHANGE_RATE = (By.XPATH, "//div[@class='current-table']/table/tbody/"
-                                    "tr/td[4]/span")
-    FROM_CURRENCY_FIELD = (By.XPATH, "//aside[@class='rates-aside']/div[1]/div[1]/"
-                                     "div[3]/div[2]")
-    TO_CURRENCY_FIELD = (By.XPATH, "//aside[@class='rates-aside']/div[1]/div[1]/"
-                                   "div[4]/div[2]")
-    FROM_CURRENCY_LIST_ELEMENT = (By.XPATH, "//aside[@class='rates-aside']/div/div/"
-                                            "div[3]/div[2]/div/div/span[%d]")
-    TO_CURRENCY_LIST_ELEMENT = (By.XPATH, "//aside[@class='rates-aside']/div/div/"
-                                          "div[4]/div[2]/div/div/span[%d]")
+    SUBMIT_BUTTON = (By.XPATH, "//aside//button")
+    RESULT_VALUE = (By.XPATH, "//h4/span[1]")
+    RESULT_CURRENCY = (By.XPATH, "//h4/span[2]")
+    BUY_EXCHANGE_RATE = (By.XPATH, "//td[3]/span")
+    SELL_EXCHANGE_RATE = (By.XPATH, "//td[4]/span")
+    FROM_CURRENCY_FIELD = (By.XPATH, "//select[@name='converterFrom']/parent::div")
+    TO_CURRENCY_FIELD = (By.XPATH, "//select[@name='converterTo']/parent::div")
+    FROM_CURRENCY_LIST_ELEMENT = (By.XPATH, "//select[@name='converterFrom']/"
+                                            "following-sibling::div/*/span[%d]")
+    TO_CURRENCY_LIST_ELEMENT = (By.XPATH, "//select[@name='converterTo']/"
+                                          "following-sibling::div/*/span[%d]")
     CONVERTER_FROM_SELECT = (By.NAME, "converterFrom")
     CONVERTER_TO_SELECT = (By.NAME, "converterTo")
 
